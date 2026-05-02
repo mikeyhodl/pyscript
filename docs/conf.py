a1335f15d785303610998a1a9d20dae357ab3283
Craig Barratt <19445341+craigbarratt@users.noreply.github.com>
2026-05-02 15:22:46 -0700

    updates for 2.0.0 release

--- docs/conf.py
+++ docs/conf.py
@@ -14,18 +14,17 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'hacs-pyscript'
-copyright = '2020-2025, Craig Barratt'
-author = 'Craig Barratt'
+project = "hacs-pyscript"
+copyright = "2020-2026, Craig Barratt"
+author = "Craig Barratt"
 
 # The full version, including alpha/beta/rc tags
-release = '1.7.0'
+release = "2.0.0"
 
-master_doc = 'index'
+master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
 
@@ -33,16 +32,16 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -55,4 +54,4 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
